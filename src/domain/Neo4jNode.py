1e9eaa5dec143b8b41439b266917a307d2d4de9f
Red Benjamin <red@benjamin.com>
2022-05-29 19:38:28 -0400

    Flask server and endpoint to create a neo4j node added

--- src/domain/Neo4jNode.py
+++ src/domain/Neo4jNode.py
@@ -7,3 +7,15 @@ class Neo4jNode:
     def __init__(self, node, name):
         self.node = node
         self.name = name
+
+    @staticmethod
+    def from_dict(json):
+        neo4j_node = Neo4jNode(node=json['node'], name=json['name'])
+        if json.get('labels') is not None:
+            neo4j_node.labels = json['labels']
+        if json.get('properties') is not None:
+            neo4j_node.properties = json['properties']
+        return neo4j_node
+
+    def __repr__(self):
+        return f"Neo4jNode(name={self.name}, node={self.node}, labels={self.labels}, properties={self.properties})"
