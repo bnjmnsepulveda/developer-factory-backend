7b27eae2a593ce7416484112b10ab09549fd9e96
Red Benjamin <red@benjamin.com>
2022-05-31 23:07:09 -0400

    Restful service added with flaska and gunicorn server

---
+++ src/main.py
@@ -0,0 +1,32 @@
+from application.create_neo4j_node import create_neo4j_node
+from domain.model.Neo4jNode import Neo4jNode
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+@app.route('/', methods=['GET'])
+def root():
+    return {
+        'app': 'Neo4j-maintainer app'
+    }
+
+
+@app.route("/neo4j/node", methods=['POST'])
+def create_neo4j_node_request():
+    body = request.get_json()
+    print(body)
+    neo4j_node = Neo4jNode.from_dict(body)
+    result = create_neo4j_node(neo4j_node)
+    print(f'Result: {result}')
+    return {
+        'message': 'OK'
+    }
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
